952d79fafae4fd01c796eb05b6e15864564558ac
tsyFcpn <95341547+tsyFcpn@users.noreply.github.com>
2022-06-20 16:05:36 -0400

    Add files via upload

--- 2/F#/Listas/Listas/Program.fs
+++ 2/F#/Listas/Listas/Program.fs
@@ -1,19 +1,22 @@
 ﻿// Más información acerca de F# en http://fsharp.net
 // Vea el proyecto "Tutorial de F#" para obtener más ayuda.
 open System
-let Listas (list:int array) n =
+let Listas (list:int []) n =
     let mutable key = true
     for i in list do
         if (n = list.[i]) then  
             if (key) then
-                printfn"%i" i
+                printfn"%i" (i + 1)
                 key <- false
     printfn"0"
 
 [<EntryPoint>]
 let main argv = 
-    let list : int array = [|1; 5; 3; 5; 6; 5; 3; 4|]
-    printfn "%A" "Criba Estructurada"
+    let list : int[] = [|1; 5; 3; 5; 6; 5; 3; 4|]
+    printfn"%A" list
+    printfn "%A" "Buscar n elemento: "
     let n = Console.ReadLine()|> int
-    Listas(list, n)
+    let temp = Listas (list) n
+
+    let n = Console.ReadKey()
     0 // devolver un código de salida entero
